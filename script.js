613cfa0f235504ff6355a8bf62b0c24087e61f33
marslikbola <mengboyevfarrux5@gmail.com>
2024-12-25 13:56:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let mevalar = [
+    'non',
+    'asal',
+    'banan',
+    'olma',
+    'aziza',
+    'Gul',
+    'mashina',
+    ];
+    
+    for (const child of mevalar) {
+        if (child.includes('n')) {
+            console.log(child + '', 'mevasida n harfi bor');
+        }
+        else{
+
+        }
+    }
